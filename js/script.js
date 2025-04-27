174550e9a06de7632ba2c444d19b878b33d7ee83
Jesús tolentino <tolentino101192@gmail.com>
2025-04-26 19:09:31 -0500

    desarrollo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 // Pequeño efecto de scroll suave para los enlaces
 document.querySelectorAll('a[href^="#"]').forEach(enlace => {
-    enlace.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+    enlace.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-  });
-  
+});
